27bc910709a6c05d9df8aeeb2d7082de41f6cab4
Captain Yoshi <captain.yoshisaur@gmail.com>
2024-10-25 16:55:06 -0400

    [impl] image resize with locked ratio

--- colony_morphology/plotting.py
+++ colony_morphology/plotting.py
@@ -7,6 +7,41 @@ import plotly.graph_objects as go
 
 from skimage import measure
 
+import cv2 as cv
+
+def image_resize(image, width = None, height = None, inter = cv.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+
+
 def plot_bboxes(image, bboxes, xywh: bool = True, labels = None) -> None:
     """
     Args:
